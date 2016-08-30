767155f5df559672c037c3756fed00d8608e830b
NanHongLemon <qoqo5555@gmail.com>
2016-08-30 16:57:45 +0800

    create TestPage

--- FireTest/app/src/main/java/com/example/lemon/firetesting/MainFirePage.java
+++ FireTest/app/src/main/java/com/example/lemon/firetesting/MainFirePage.java
@@ -1,13 +1,29 @@
 package com.example.lemon.firetesting;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainFirePage extends AppCompatActivity {
+
+    private Button startTesting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_fire_page);
+
+        startTesting = (Button) findViewById(R.id.StartTesting);
+        startTesting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainFirePage.this, TestingPage.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ FireTest/app/src/main/java/com/example/lemon/firetesting/TestingPage.java
@@ -0,0 +1,22 @@
+package com.example.lemon.firetesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TestingPage extends AppCompatActivity {
+
+    private ArrayList Ans = new ArrayList();
+    private HashMap Exam = new HashMap();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testing_page);
+
+        
+
+    }
+}
